e9f5ef68a541d64ccb177aa2ac5d3f7e980b5be2
t.gusev <gusev.t.s1@gmail.com>
2022-07-07 18:43:36 +0300

    run.sh improved

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ docker run \
     --restart=always \
     --network="dev" \
     --name=htracker-haskell \
+    -v /home/apps/tracker-haskell/logs:/cardano-markets-tracker/logs \
     -d timooxaaa/htracker-cardano:0.0.1
